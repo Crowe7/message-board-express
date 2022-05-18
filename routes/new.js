3bf6129e38c2ccb889c08ef5143ab69ef68853f2
Crowe7 <jacobocrowe@gmail.com>
2022-05-18 03:02:24 -0500

    added POST to new router to allow user to submit post to the index page && moved the messages to own file

--- routes/new.js
+++ routes/new.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
-
+var messages = require('../public/javascripts/messages');
 /* GET new post form. */
 router.get('/', function(req, res, next) {
     res.render('form');
 });
+
+router.post('/', function(req, res, next) {
+    const MessageContent = req.body;
+
+    messages.push({user: MessageContent.user, text: MessageContent.message, added: new Date()});
+
+    res.redirect('/');
+})
 
 module.exports = router;
